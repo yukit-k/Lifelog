bc18136fb79eccb2946e30e68fde1cb9e87ae9b5
yuki678 <uk.12juni@gmail.com>
2021-01-02 16:03:04 +0000

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Yuki Takahashi on 02/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
