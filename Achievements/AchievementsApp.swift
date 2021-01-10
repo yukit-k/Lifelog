cb2c0ed76961c80d7eeeaf20b4feaec4bfb910d0
yuki678 <uk.12juni@gmail.com>
2021-01-10 11:18:52 +0000

    Initial Files

---
+++ Achievements/AchievementsApp.swift
@@ -0,0 +1,21 @@
+//
+//  AchievementsApp.swift
+//  AchievementsApp
+//
+//  Created by Yuki Takahashi on 02/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct AchievementsApp: App {
+    @StateObject private var modelData = ModelData()
+    @StateObject var viewRouter = ViewRouter()
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView(viewRouter: viewRouter)
+                .environmentObject(modelData)
+        }
+    }
+}
