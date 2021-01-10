6b4c7f294f8673fea1f8f1a44e3947c4047ca5b5
yuki678 <uk.12juni@gmail.com>
2021-01-10 11:28:52 +0000

    Add Landmark View

---
+++ Achievements/Views/Landmarks/LandmarkList.swift
@@ -0,0 +1,51 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Yuki Takahashi on 02/01/2021.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    @EnvironmentObject var modelData: ModelData
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        modelData.landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
+                }
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var landmarks = ModelData().landmarks
+    
+    static var previews: some View {
+        /*
+        ForEach(["iPhone SE", "iPhone XS Max"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
+        */
+        LandmarkList()
+            .environmentObject(ModelData())
+    }
+}
