76cd65d5ea64ba64b854f40f513067b473bbe721
yuki678 <uk.12juni@gmail.com>
2021-01-17 07:48:44 +0000

    Update the lifecycle to UIKit from Swiftui

--- Achievements/AchievementsApp.swift
+++ Achievements/AchievementsApp.swift
@@ -5,17 +5,17 @@
 //  Created by Yuki Takahashi on 02/01/2021.
 //
 
-import SwiftUI
-
-@main
-struct AchievementsApp: App {
-    @StateObject private var modelData = ModelData()
-    @StateObject var viewRouter = ViewRouter()
-    
-    var body: some Scene {
-        WindowGroup {
-            ContentView(viewRouter: viewRouter)
-                .environmentObject(modelData)
-        }
-    }
-}
+//import SwiftUI
+//
+//@main
+//struct AchievementsApp: App {
+//    @StateObject private var modelData = ModelData()
+//    @StateObject var viewRouter = ViewRouter()
+//
+//    var body: some Scene {
+//        WindowGroup {
+//            ContentView(viewRouter: viewRouter)
+//                .environmentObject(modelData)
+//        }
+//    }
+//}
--- Achievements/SceneDelegate.swift
+++ Achievements/SceneDelegate.swift
@@ -11,6 +11,8 @@ import SwiftUI
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    @StateObject var viewRouter = ViewRouter()
+    @StateObject private var modelData = ModelData()
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
@@ -23,7 +25,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
         // Create the SwiftUI view and set the context as the value for the managedObjectContext environment keyPath.
         // Add `@Environment(\.managedObjectContext)` in the views that will need the context.
-        let contentView = ContentView().environment(\.managedObjectContext, context)
+        //let contentView = ContentView(viewRouter: self.viewRouter)
+        let contentView = BookList()
+            .environment(\.managedObjectContext, context)
+            //.environmentObject(modelData)
 
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
