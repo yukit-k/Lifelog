ca3464de1e6bf7a073d8e0a964763a4a3495d972
yuki678 <uk.12juni@gmail.com>
2021-01-18 07:51:21 +0000

    Create an Image Picker (camera or photo library)

---
+++ Achievements/Views/ImagePicker.swift
@@ -0,0 +1,63 @@
+//
+//  ImagePicker.swift
+//  Achievements
+//
+//  Created by Yuki Takahashi on 17/01/2021.
+//
+
+import SwiftUI
+import UIKit
+
+struct ImagePicker: UIViewControllerRepresentable {
+    class Coordicator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        var parent: ImagePicker
+        
+//        @Binding var image: Image?
+//        @Binding var isPresented: Bool
+        
+    //    init(image: Binding<Image?>, isPresented: Binding<Bool>) {
+    //        self._image = image
+    //        self._isPresented = isPresented
+    //    }
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let uiImage = info[.originalImage] as? UIImage {
+                parent.image = uiImage
+            }
+            //parent.presentationMode.wrappedValue.dismiss()
+            parent.isPresented = false
+        }
+        
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            //parent.presentationMode.wrappedValue.dismiss()
+            parent.isPresented = false
+        }
+
+    }
+    
+    var sourceType: UIImagePickerController.SourceType = .camera
+    @Binding var image: UIImage?
+    //@Environment(\.presentationMode) var presentationMode
+    @Binding var isPresented: Bool
+    
+    func makeCoordinator() -> Coordicator {
+        //return BookImagePickerCoordicator(image: $image, isPresented: $isPresented)
+        Coordicator(self)
+    }
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) ->  UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.sourceType = sourceType
+        picker.delegate = context.coordinator
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePicker>) {
+        // something
+    }
+}
+
+
