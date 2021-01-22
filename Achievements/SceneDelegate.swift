67fbb1693be88f6bf6d0d8fd88b9570eaf1343c2
yuki678 <uk.12juni@gmail.com>
2021-01-22 16:16:13 +0000

    Add merge policy to the context in SceneDelegate

--- Achievements/SceneDelegate.swift
+++ Achievements/SceneDelegate.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Yuki Takahashi on 16/01/2021.
 //
-
+import CoreData
 import UIKit
 import SwiftUI
 
@@ -22,13 +22,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
         // Get the managed object context from the shared persistent container.
         let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        context.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
 
         // Create the SwiftUI view and set the context as the value for the managedObjectContext environment keyPath.
         // Add `@Environment(\.managedObjectContext)` in the views that will need the context.
         let contentView = ContentView(viewRouter: self.viewRouter)
         // let contentView = BookList()
             .environment(\.managedObjectContext, context)
-            //.environmentObject(modelData)
+            .environmentObject(modelData)
 
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
