bff7bd1d4634774faf01df42220ad490dca98905
yuki678 <uk.12juni@gmail.com>
2021-01-22 16:33:22 +0000

    Update the view layout

--- Achievements/Views/Material View/MaterialView.swift
+++ Achievements/Views/Material View/MaterialView.swift
@@ -7,28 +7,49 @@
 
 import SwiftUI
 
-struct Highlight: View {
+struct MaterialView: View {
     @EnvironmentObject var modelData: ModelData
+    @Environment(\.managedObjectContext) var moc
+//    @FetchRequest(entity: Material.entity(), sortDescriptors: [
+//        NSSortDescriptor(keyPath: \Material.updateDate, ascending: false)
+//    ]) var materials: FetchedResults<Material>
+
     @State private var showingProfile = false
+    private let topImages = [
+        "silversalmoncreek",
+        "chilkoottrail",
+        "stmarylake",
+        "twinlake",
+        "lakemcdonald",
+        "charleyrivers",
+        "icybay",
+        "rainbowlake",
+        "hiddenlake",
+        "chincoteague",
+        "umbagog"
+    ]
+    //private let imageIndex = Int.random(in: 0..<11)
     
     var body: some View {
         NavigationView {
             List {
-                modelData.features[0].image
+                Image(topImages[4])
                     .resizable()
                     .scaledToFill()
                     .frame(height: 200)
                     .clipped()
                     .listRowInsets(EdgeInsets())
-
-                ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
+                
+                ForEach(Material.Category.allCases) { category in
+//                ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
 //                    CategoryRow(categoryName: key, items: modelData.categories[key]!)
-                    CategoryRow(categoryName: key)
+                    CategoryRow(filter: category.rawValue)
                 }
                 .listRowInsets(EdgeInsets())
             }
             .listStyle(InsetListStyle())
-            .navigationTitle("Highlight")
+            .navigationTitle("Materaials")
+            //.navigationBarHidden(true)
             .toolbar {
                 Button(action: { showingProfile.toggle() }) {
                     Image(systemName: "person.crop.circle")
@@ -45,7 +66,7 @@ struct Highlight: View {
 
 struct Highlight_Previews: PreviewProvider {
     static var previews: some View {
-        Highlight()
+        MaterialView()
             .environmentObject(ModelData())
     }
 }
