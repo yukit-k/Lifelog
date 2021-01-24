37d9d80799bd4a438eafae28a02052e1bc6c0877
yuki678 <uk.12juni@gmail.com>
2021-01-24 00:37:59 +0000

    Add Search Bar

---
+++ Achievements/Views/UIImageExtension.swift
@@ -0,0 +1,29 @@
+//
+//  UIImageExtension.swift
+//  Achievements
+//
+//  Created by Yuki Takahashi on 23/01/2021.
+//
+
+import SwiftUI
+import Foundation
+
+extension UIImage {
+    func resized(withPercentage percentage: CGFloat, isOpaque: Bool = true) -> UIImage? {
+        let canvas = CGSize(width: size.width * percentage, height: size.height * percentage)
+        let format = imageRendererFormat
+        format.opaque = isOpaque
+        return UIGraphicsImageRenderer(size: canvas, format: format).image {
+            _ in draw(in: CGRect(origin: .zero, size: canvas))
+        }
+    }
+    func resized(toWidth width: CGFloat, isOpaque: Bool = true) -> UIImage? {
+        let canvas = CGSize(width: width, height: CGFloat(ceil(width/size.width * size.height)))
+        let format = imageRendererFormat
+        format.opaque = isOpaque
+        return UIGraphicsImageRenderer(size: canvas, format: format).image {
+            _ in draw(in: CGRect(origin: .zero, size: canvas))
+        }
+    }
+    
+}
