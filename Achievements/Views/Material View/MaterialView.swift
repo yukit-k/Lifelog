44429fbbb484aedc140c87427b15caf6b538e330
yuki678 <uk.12juni@gmail.com>
2021-01-25 17:56:50 +0000

    Layout and top picture updated

--- Achievements/Views/Material View/MaterialView.swift
+++ Achievements/Views/Material View/MaterialView.swift
@@ -8,19 +8,16 @@
 import SwiftUI
 
 struct MaterialView: View {
-    @EnvironmentObject var modelData: ModelData
+    @EnvironmentObject var startupData: StartupData
     @Environment(\.managedObjectContext) var moc
-//    @FetchRequest(entity: Material.entity(), sortDescriptors: [
-//        NSSortDescriptor(keyPath: \Material.updateDate, ascending: false)
-//    ]) var materials: FetchedResults<Material>
 
     @State private var showingProfile = false
-    private let topImages = "mountains-190056"
+    private let topImages = ["mountains-190056", "mountains-2031539"]
     
     var body: some View {
         NavigationView {
             List {
-                Image(topImages)
+                Image(topImages[startupData.photoIndex])
                     .resizable()
                     .scaledToFill()
                     .frame(height: 200)
@@ -38,14 +35,21 @@ struct MaterialView: View {
             .navigationTitle("Achievement")
             //.navigationBarHidden(true)
             .toolbar {
-                Button(action: { showingProfile.toggle() }) {
-                    Image(systemName: "person.crop.circle")
-                        .accessibilityLabel("User Profile")
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: { showingProfile.toggle() }) {
+                        Image(systemName: "person.crop.circle")
+                            .accessibilityLabel("User Profile")
+                    }
+                }
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button(action: {}) {
+                        Image(systemName: "gearshape")
+                    }
                 }
             }
             .sheet(isPresented: $showingProfile) {
                 ProfileHost()
-                    .environmentObject(modelData)
+                    .environmentObject(startupData)
             }
         }
     }
@@ -54,6 +58,6 @@ struct MaterialView: View {
 struct Highlight_Previews: PreviewProvider {
     static var previews: some View {
         MaterialView()
-            .environmentObject(ModelData())
+            .environmentObject(StartupData())
     }
 }
