1a81c4decb4f6d9b526828c406cd983c3b178194
yuki678 <uk.12juni@gmail.com>
2021-01-25 17:58:24 +0000

    Change category title to use Material.CategoryIcon + Material.Category and change destination view

--- Achievements/Views/Material View/CategoryRow.swift
+++ Achievements/Views/Material View/CategoryRow.swift
@@ -12,21 +12,22 @@ struct CategoryRow: View {
 //    @Environment(\.managedObjectContext) var moc
     var fetchRequest: FetchRequest<Material>
     var categoryName: String
+    var categoryNameWithIcon: String
 //    @FetchRequest(entity: Material.entity(), sortDescriptors: [
 //        NSSortDescriptor(keyPath: \Material.updateDate, ascending: false)
 //    ]) var materials: FetchedResults<Material>
 
     var body: some View {
         VStack(alignment: .leading) {
-            Text(categoryName)
+            Text(categoryNameWithIcon)
                 .font(.headline)
                 .padding(.leading, 15)
-                .padding(.top, 5)
+                .padding(.top, 15)
             
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack(alignment: .top, spacing: 0) {
                     ForEach(fetchRequest.wrappedValue, id: \.self) { material in
-                        NavigationLink(destination: MaterialDetailBook(material: material)) {
+                        NavigationLink(destination: ItemDetailBook(material: material)) {
                             CategoryItem(material: material)
                         }
                     }
@@ -40,12 +41,17 @@ struct CategoryRow: View {
         fetchRequest = FetchRequest<Material>(entity: Material.entity(), sortDescriptors: [ NSSortDescriptor(keyPath: \Material.updateDate, ascending: false)
         ], predicate: NSPredicate(format: "category == %@", filter))
         categoryName = filter
+        switch filter {
+        case Material.Category.Book.rawValue: categoryNameWithIcon = Material.CategoryIcon.Book.rawValue + filter
+        case Material.Category.Task.rawValue: categoryNameWithIcon = Material.CategoryIcon.Task.rawValue + filter
+        case Material.Category.Cook.rawValue: categoryNameWithIcon = Material.CategoryIcon.Cook.rawValue + filter
+        case Material.Category.Exercise.rawValue: categoryNameWithIcon = Material.CategoryIcon.Exercise.rawValue + filter
+        default: categoryNameWithIcon = Material.CategoryIcon.Others.rawValue + filter
+        }
     }
 }
 
-struct CategoryRow_Previews: PreviewProvider {
-    static var landmarks = ModelData().landmarks
-    
+struct CategoryRow_Previews: PreviewProvider {    
     static var previews: some View {
         CategoryRow(filter: "Book")
     }
