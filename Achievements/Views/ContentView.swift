a28b33d2cb9512eaf647cf30d1fc1c4b6730d670
yuki678 <uk.12juni@gmail.com>
2021-01-31 15:50:20 +0000

    Update the layout to have a dynamic category buttons

--- Achievements/Views/ContentView.swift
+++ Achievements/Views/ContentView.swift
@@ -8,75 +8,69 @@
 import SwiftUI
 
 struct ContentView: View {
-    @EnvironmentObject var startupData: StartupData
+    @EnvironmentObject var modelData: ModelData
     @StateObject var viewRouter: ViewRouter
     
     var body: some View {
         GeometryReader { geometry in
             VStack {
-                Spacer()
                 switch viewRouter.currentTab {
                 case .highlight:
                     PictureView()
-                        .environmentObject(startupData)
+//                        .frame(width: geometry.size.width, height: geometry.size.height - 50)
                 case .list:
                     ListView()
                 case .chart:
                     ChartView()
                 case .calendar:
-                    Text("Calendar View")
+                    CalendarView()
                 }
-                
-                Spacer()
-                ZStack {
+   
+                ZStack(alignment: .bottom) {
                     if viewRouter.showPopup {
-                        PlusMenu(viewRouter: viewRouter, widthAndHeight: geometry.size.width/6)
-                            .offset(y: -geometry.size.height/6)
-                            
+                        PlusMenu(viewRouter: viewRouter, widthAndHeight: 70)
+                            .offset(y: -150)
                     }
-                    HStack {
-                        TabBarIcon(viewRouter: viewRouter, assignedTab: .highlight, width: geometry.size.width/5, height: geometry.size.height/32, systemIconName: "star", tabName: "Achievement")
-                        TabBarIcon(viewRouter: viewRouter, assignedTab: .list, width: geometry.size.width/5, height: geometry.size.height/32, systemIconName: "list.bullet", tabName: "Activity")
-                        ZStack {
-                            Circle()
-                                .foregroundColor(.white)
-                                .frame(width: geometry.size.width/7, height: geometry.size.width/7)
-                                .shadow(radius: 4)
-                            Image(systemName: "plus.circle.fill")
-                                .resizable()
-                                .frame(width: geometry.size.width/7-6, height: geometry.size.width/7-6)
-                                .foregroundColor(.accentColor)
-                                .rotationEffect(Angle(degrees: viewRouter.showPopup ? 90 : 0))
-                        }
-                            .offset(y: -geometry.size.height/10/2.5)
-                        .onTapGesture {
-                            withAnimation {
-                                viewRouter.showPopup.toggle()
+                    HStack() {
+                            TabBarIcon(viewRouter: viewRouter, assignedTab: .highlight, width: geometry.size.width/5, height: 15, systemIconName: "star", tabName: "Achievement")
+                            TabBarIcon(viewRouter: viewRouter, assignedTab: .list, width: geometry.size.width/5, height: 15, systemIconName: "list.bullet", tabName: "Activity")
+                            ZStack {
+                                Circle()
+                                    .foregroundColor(.white)
+                                    .frame(width: 50, height: 50)
+                                    .shadow(radius: 4)
+                                Image(systemName: "plus.circle.fill")
+                                    .resizable()
+                                    .frame(width: 50-6, height: 50-6)
+                                    .foregroundColor(.accentColor)
+                                    .rotationEffect(Angle(degrees: viewRouter.showPopup ? 90 : 0))
                             }
-                        }
-                        TabBarIcon(viewRouter: viewRouter, assignedTab: .chart, width: geometry.size.width/5, height: geometry.size.height/32, systemIconName: "chart.bar", tabName: "Chart")
-                        TabBarIcon(viewRouter: viewRouter, assignedTab: .calendar, width: geometry.size.width/5, height: geometry.size.height/32, systemIconName: "calendar", tabName: "Calendar")
-                     }
-                         .frame(width: geometry.size.width, height: geometry.size.height/10)
-                    .background(Color(.secondarySystemBackground).shadow(radius: 2))
+                                .offset(y: -25)
+                            .onTapGesture {
+                                withAnimation {
+                                    viewRouter.showPopup.toggle()
+                                }
+                            }
+                            TabBarIcon(viewRouter: viewRouter, assignedTab: .chart, width: geometry.size.width/5, height: 15, systemIconName: "chart.bar", tabName: "Chart")
+                            TabBarIcon(viewRouter: viewRouter, assignedTab: .calendar, width: geometry.size.width/5, height: 15, systemIconName: "calendar", tabName: "Calendar")
+                         }
+                        .frame(width: geometry.size.width, height: 50)
+//                        .position(y:geometry.size.height - 50)
+                        .background(Color(.secondarySystemBackground).shadow(radius: 2))
+                    }
                 }
                 .edgesIgnoringSafeArea(.all)
             }
             
             .sheet(isPresented: $viewRouter.showAddSheet) {
-                switch viewRouter.activeSheet {
-                case .first:
-                    AddNewLog(isToDo: true)
-                case .second:
-                    AddNewLog(isToDo: false)
-                }
-            }
+                AddNewLog(category: viewRouter.category)
+            
         }
     }
 }        
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView(viewRouter: ViewRouter())
-            .environmentObject(StartupData())
+            .environmentObject(ModelData())
     }
 }
