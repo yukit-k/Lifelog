e72651969012baf848ce9314c7700120babdc9a1
yuki678 <uk.12juni@gmail.com>
2021-01-31 15:51:14 +0000

    Dynamically read category from EnvironmentObject to create buttons

--- Achievements/Views/PlusMenu.swift
+++ Achievements/Views/PlusMenu.swift
@@ -9,53 +9,59 @@ import SwiftUI
 
 struct PlusMenu: View {
     @StateObject var viewRouter: ViewRouter
+    @EnvironmentObject var modelData: ModelData
+    
     let widthAndHeight: CGFloat
+    let cols: Int = 4
+    let spacing: CGFloat = 30
+    
     var body: some View {
-        HStack(spacing: 50) {
-            ZStack {
-                Circle()
-                    .foregroundColor(.accentColor)
-                    .frame(width: widthAndHeight, height: widthAndHeight)
-                    .shadow(radius: 4)
-                VStack {
-                    Image(systemName: "alarm")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .padding(.top, widthAndHeight/6)
-                    Text("To Do")
-                        .font(.footnote)
-                        .padding(.bottom, widthAndHeight/10)
+        let gridItems = Array(repeating: GridItem(.fixed(widthAndHeight), spacing: spacing, alignment: .center), count: cols)
+        LazyVGrid(columns: gridItems, alignment: .center, spacing: spacing) {
+            ForEach(modelData.userSettings.categories, id: \.self) { category in
+                ZStack {
+                    Circle()
+                        .foregroundColor(.accentColor)
+                        .frame(width: widthAndHeight, height: widthAndHeight)
+                        .shadow(radius: 4)
+                    VStack {
+                        Text(category.icon ?? "")
+                            .font(.title)
+                        Text(category.name)
+                            .font(.footnote)
+                            .padding(.bottom, widthAndHeight/20)
+                    }
+                        .foregroundColor(.white)
+                        .frame(width: widthAndHeight, height: widthAndHeight)
                 }
-                    .foregroundColor(.white)
-                    .frame(width: widthAndHeight, height: widthAndHeight)
-            }
-            .onTapGesture {
-                viewRouter.showPopup = false
-                viewRouter.activeSheet = .first
-                viewRouter.showAddSheet = true
-            }
-            ZStack {
-                Circle()
-                    .foregroundColor(.accentColor)
-                    .frame(width: widthAndHeight, height: widthAndHeight)
-                    .shadow(radius: 4)
-                VStack {
-                    Image(systemName: "text.badge.checkmark")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .padding(.top, widthAndHeight/6)
-                    Text("Log")
-                        .font(.footnote)
-                        .padding(.bottom, widthAndHeight/10)
+                .onTapGesture {
+                    viewRouter.showPopup = false
+                    viewRouter.category = category
+                    viewRouter.showAddSheet = true
                 }
-                    .foregroundColor(.white)
-                    .frame(width: widthAndHeight, height: widthAndHeight)
             }
-            .onTapGesture {
-                viewRouter.showPopup = false
-                viewRouter.activeSheet = .second
-                viewRouter.showAddSheet = true
-            }
+//            ZStack {
+//                Circle()
+//                    .foregroundColor(.accentColor)
+//                    .frame(width: widthAndHeight, height: widthAndHeight)
+//                    .shadow(radius: 4)
+//                VStack {
+//                    Image(systemName: "text.badge.checkmark")
+//                        .resizable()
+//                        .aspectRatio(contentMode: .fit)
+//                        .padding(.top, widthAndHeight/6)
+//                    Text("Log")
+//                        .font(.footnote)
+//                        .padding(.bottom, widthAndHeight/10)
+//                }
+//                    .foregroundColor(.white)
+//                    .frame(width: widthAndHeight, height: widthAndHeight)
+//            }
+//            .onTapGesture {
+//                viewRouter.showPopup = false
+//                viewRouter.activeSheet = .second
+//                viewRouter.showAddSheet = true
+//            }
         }
             .transition(.scale)
     }
