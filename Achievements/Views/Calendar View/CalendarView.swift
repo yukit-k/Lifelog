2b95e697fb4590211ad1039bef69c184707cc706
yuki678 <uk.12juni@gmail.com>
2021-01-31 15:49:45 +0000

    Add a new view (Calendar View)

---
+++ Achievements/Views/Calendar View/CalendarView.swift
@@ -0,0 +1,20 @@
+//
+//  CalendarView.swift
+//  Achievements
+//
+//  Created by Yuki Takahashi on 31/01/2021.
+//
+
+import SwiftUI
+
+struct CalendarView: View {
+    var body: some View {
+        Text("Coming soon!")
+    }
+}
+
+struct CalendarView_Previews: PreviewProvider {
+    static var previews: some View {
+        CalendarView()
+    }
+}
