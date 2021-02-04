c3cac89b2f106b297695be6c0a0fb3abd0b4c428
yuki678 <uk.12juni@gmail.com>
2021-02-04 13:06:48 +0000

    Create a new Category Setting View to update the category

---
+++ Achievements/Views/Profile/EmojiTextField.swift
@@ -0,0 +1,55 @@
+//
+//  EmojiTextField.swift
+//  Achievements
+//
+//  Created by Yuki Takahashi on 04/02/2021.
+//
+
+import SwiftUI
+
+struct TextFieldWrapperView: UIViewRepresentable {
+
+    @Binding var text: String
+
+    func makeCoordinator() -> TFCoordinator {
+        TFCoordinator(self)
+    }
+}
+
+extension TextFieldWrapperView {
+
+
+    func makeUIView(context: UIViewRepresentableContext<TextFieldWrapperView>) -> UITextField {
+        let textField = EmojiTextField()
+        textField.delegate = context.coordinator
+        return textField
+    }
+
+
+    func updateUIView(_ uiView: UITextField, context: Context) {
+
+    }
+}
+
+class TFCoordinator: NSObject, UITextFieldDelegate {
+    var parent: TextFieldWrapperView
+
+    init(_ textField: TextFieldWrapperView) {
+        self.parent = textField
+    }
+}
+
+class EmojiTextField: UITextField {
+
+   // required for iOS 13
+   override var textInputContextIdentifier: String? { "" } // return non-nil to show the Emoji keyboard ¯\_(ツ)_/¯
+
+    override var textInputMode: UITextInputMode? {
+        for mode in UITextInputMode.activeInputModes {
+            if mode.primaryLanguage == "emoji" {
+                return mode
+            }
+        }
+        return nil
+    }
+}
