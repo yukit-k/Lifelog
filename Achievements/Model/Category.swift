b257acac859c1181fe45a97eab2abb3dca25e4be
yuki678 <uk.12juni@gmail.com>
2021-02-04 13:07:20 +0000

    Create a new Category Setting View to update the category

--- Achievements/Model/Category.swift
+++ Achievements/Model/Category.swift
@@ -7,10 +7,67 @@
 
 import Foundation
 
-struct Category: Identifiable, Hashable {
+struct Category: Identifiable, Codable, Hashable {
     var id = UUID()
     var name: String
     var icon: String?
     var unit: String?
-    var subCategories: [Category]?
+    var subCategories: [SubCategory]
+    
+    var wrappedIcon: String {
+        icon ?? ""
+    }
+    var wrappedUnit: String {
+        unit ?? ""
+    }
+}
+
+struct SubCategory: Identifiable, Codable, Hashable {
+    var id = UUID()
+    var name: String
+    var icon: String?
+    var unit: String?
+    
+    var wrappedIcon: String {
+        icon ?? ""
+    }
+    var wrappedUnit: String {
+        unit ?? ""
+    }
+
+}
+
+class CategoryItem: ObservableObject {
+    @Published var category: Category
+    @Published var categoryIndex: Int?
+    @Published var subCategory: SubCategory
+    @Published var subCategoryIndex: Int?
+    
+    
+    init(category: Category, categoryIndex: Int? = nil, subCategory: SubCategory = SubCategory(name: ""), subCategoryIndex: Int? = nil) {
+        self.category = category
+        self.categoryIndex = categoryIndex
+        self.subCategory = subCategory
+        self.subCategoryIndex = subCategoryIndex
+    }
+    
+    
+    init() {
+        self.category = Category(name: "", subCategories: [SubCategory(name: "")])
+//        self.categoryIndex = 0
+        self.subCategory = SubCategory(name: "")
+//        self.subCategoryIndex = 0
+    }
+    
+//    func setCategory(category: Category, index: Int? = nil) -> CategoryItem {
+//        self.category = category
+//        self.categoryIndex = index
+//        return self
+//    }
+//
+//    func addSubCategory(subCategory: SubCategory, index: Int? = nil) -> CategoryItem {
+//        self.subCategory = subCategory
+//        self.subCategoryIndex = index
+//        return self
+//    }
 }
