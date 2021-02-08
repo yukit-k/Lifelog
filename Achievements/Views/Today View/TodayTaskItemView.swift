7976824738fa7fc9a6d9e7aab81aae816f0829fe
yuki678 <uk.12juni@gmail.com>
2021-02-08 23:37:00 +0000

    Add Today's To Do View

---
+++ Achievements/Views/Today View/TodayTaskItemView.swift
@@ -0,0 +1,66 @@
+//
+//  TodayTaskItemView.swift
+//  Achievements
+//
+//  Created by Yuki Takahashi on 07/02/2021.
+//
+
+import SwiftUI
+
+struct TodayTaskItemView: View {
+    @ObservedObject var log: Log
+    var isCompleted: Bool
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 5)
+                .stroke(isCompleted ? Color.blue : Color.primary, lineWidth: 1)
+                .background(isCompleted ? Color.blue : Color.white)
+                .frame(height: 60)
+            HStack {
+                log.image.map({
+                    UIImage(data: $0)
+                        .map({
+                                Image(uiImage: $0)
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width:45, height:45)
+                                    .clipped()
+                                    .padding(.leading, 5)
+                        })
+                })
+                VStack(alignment: .leading) {
+                    HStack {
+                        if isCompleted {
+                            Image(systemName: "checkmark")
+                                .foregroundColor(.white)
+                        }
+                        Text(log.wrappedName)
+                            .font(.headline)
+                            .foregroundColor(isCompleted ? .white : .primary)
+                        Spacer()
+                    }
+                    Text("\(log.wrappedCategoryIcon)\(log.wrappedCategory) / \(log.wrappedSubCategory)")
+                        .font(.caption)
+                        .foregroundColor(isCompleted ? .white : .primary)
+                }
+                Spacer()
+            }
+        }
+    }
+}
+
+struct TodayTaskItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let log1 = Log(context: context)
+        log1.name = "Test book"
+        log1.category = "Book"
+        log1.subCategory = "Fantasy"
+        log1.image = UIImage(named: "defaultBook")?.pngData()
+        log1.rating = 4
+        log1.comment = "This was a great book"
+        log1.activityDate = Date()
+        log1.updatedDate = Date()
+        return TodayTaskItemView(log: log1, isCompleted: false).environment(\.managedObjectContext, context)
+    }
+}
--- Achievements/Views/Today View/TodayView.swift
+++ Achievements/Views/Today View/TodayView.swift
@@ -13,46 +13,126 @@ struct TodayView: View {
     @State private var activeSheet: ActiveSheetNavBar?
     @State private var isFiltered: Bool = false
     @StateObject var filterCategory = CategoryItem()
-
-    let testData = ["Math", "English", "Japanese", "Mummy", "Daddy", "TTR", "IDL"]
-    @State private var isCompleted: [Bool]
     
-    init() {
-        _isCompleted = State(initialValue: Array(repeating: false, count: testData.count))
+    var dateFormatter: DateFormatter {
+            let formatter = DateFormatter()
+            formatter.dateStyle = .short
+            return formatter
     }
+    
+    @FetchRequest(entity: Log.entity(),
+                  sortDescriptors: [
+                    NSSortDescriptor(keyPath: \Log.category, ascending: true),
+                    NSSortDescriptor(keyPath: \Log.subCategory, ascending: true),
+                    NSSortDescriptor(keyPath: \Log.name, ascending: true)
+                  ],
+                  predicate: NSCompoundPredicate(type: .and, subpredicates: [
+                      NSPredicate(format: "activityDate <= %@", Date() as CVarArg),
+                      NSPredicate(format: "isRoutine == true")
+                  ])) var routineLogs: FetchedResults<Log>
+    
+//    @FetchRequest var fetchRequestRoutine: FetchedResults
+    //@FetchRequest var filteredRoutineDone: FetchedResults
+//    init() {
+//        self._fetchRequestRoutine = FetchRequest(
+//            Entity: Log.entity(),
+//        )
+//    }
+
+//    var fetchRequestRoutine = FetchRequest<Log>(entity: Log.entity(),
+//                        sortDescriptors: [],
+//                        predicate: NSCompoundPredicate(type: .and, subpredicates: [
+//                            NSPredicate(format: "activityDate <= %@", Date() as CVarArg),
+//                            NSPredicate(format: "isRoutine == true")
+//                        ])
+//    )
+//    var routineLogs: FetchedResults<Log> { fetchRequestRoutine.wrappedValue }
+
+    var filteredRoutineTodo: [Log] {
+        routineLogs.filter { log in
+            (log.isToDo) &&
+            ((isFiltered == false) ||
+                (isFiltered == true && log.wrappedCategory == filterCategory.category.name)
+            )
+        }
+    }
+    var filteredRoutineDone: [Log] {
+        routineLogs.filter { log in
+            (!log.isToDo && dateFormatter.string(from: log.wrappedActivityDate) == dateFormatter.string(from: Date())) &&
+            ((isFiltered == false) ||
+                (isFiltered == true && log.wrappedCategory == filterCategory.category.name)
+            )
+        }
+    }
+
+    @FetchRequest(entity: Log.entity(),
+                  sortDescriptors: [
+                    NSSortDescriptor(keyPath: \Log.name, ascending: true)
+                  ],
+                  predicate: NSCompoundPredicate(type: .and, subpredicates: [
+                      NSPredicate(format: "activityDate >= %@", Date().addingTimeInterval(-60*60*24*2) as CVarArg),
+                      NSPredicate(format: "isRoutine == false")
+                  ])) var adhocLogs: FetchedResults<Log>
+
+    var filteredAdhocLogs: [Log] {
+        adhocLogs.filter { log in
+            let activityDate = dateFormatter.string(from: log.wrappedActivityDate)
+            let today = dateFormatter.string(from: Date())
+            return (activityDate == today) &&
+                (isFiltered == false ||
+                    isFiltered == true && log.wrappedCategory == filterCategory.category.name
+                )
+        }
+    }
+    
+    @State var stateRoutineTodo = [Log]()
+    @State var stateRoutineDone = [Log]()
+    @State var stateAdhocLogs = [Log]()
+
     var body: some View {
         GeometryReader { geometry in
             NavigationView {
                 ScrollView {
+                    if isFiltered {
+                        Text("Applied Fileter: \(filterCategory.category.icon ?? "")\(filterCategory.category.name)")
+                            .padding(.top, 20)
+                            .padding(.bottom, 0)
+                    }
                     let gridItems = Array(repeating: GridItem(.fixed(geometry.size.width/2), spacing: 0, alignment: .center), count: 2)
-                    LazyVGrid(columns: gridItems, alignment: .center, spacing: 10) {
-                        ForEach(testData.indices, id: \.self) { index in
-                            ZStack {
-                                RoundedRectangle(cornerRadius: 5)
-                                    .stroke(Color.primary, lineWidth: 1)
-                                    .background(isCompleted[index] ? Color.blue : Color.white)
-                                if isCompleted[index] {
-                                    HStack {
-                                        Image(systemName: "checkmark")
-                                            .foregroundColor(.white)
-                                        Text(testData[index])
-                                            .foregroundColor(.white)
+                    Section(header: Text("Daily Routine").font(.title2).fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/).padding(.top, 10)) {
+                        LazyVGrid(columns: gridItems, alignment: .center, spacing: 10) {
+                            ForEach(Array(stateRoutineTodo.enumerated()), id: \.1.id) { index, log in
+                                TodayTaskItemView(log: log, isCompleted: stateRoutineDone.firstIndex(where: {$0.routineLogId == log.id}) != nil)
+//                                    .frame(height: 80)
+                                    .padding(.horizontal, 10)
+                                    .padding(.top, 10)
+                                    .onTapGesture {
+                                        if (stateRoutineDone.firstIndex(where: {$0.routineLogId == log.id}) != nil) {
+                                            self.delete(log: log)
+                                        } else {
+                                            self.add(log: log)
+                                        }
                                     }
-                                } else {
-                                    Text(testData[index])
-                                }
                             }
-                            .frame(height: 100)
-                            .padding(.horizontal, 10)
-                            .padding(.top, 10)
-                            .onTapGesture {
-                                isCompleted[index].toggle()
+                        }
+                    }
+                    Divider()
+                    Section(header: Text("Specific for Today").font(.title2).fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/).padding(.top, 10)) {
+                        LazyVGrid(columns: gridItems, alignment: .center, spacing: 10) {
+                            ForEach(Array(filteredAdhocLogs.enumerated()), id: \.1.id) { index, log in
+                                TodayTaskItemView(log: log, isCompleted: !log.isToDo)
+//                                    .frame(height: 80)
+                                    .padding(.horizontal, 10)
+                                    .padding(.top, 10)
+                                    .onTapGesture {
+                                        self.update(log: log)
+                                    }
                             }
                         }
                     }
                 }
                 .navigationBarTitleDisplayMode(.inline)
-                .navigationBarTitle("Today's To Do")
+                .navigationBarTitle("Today's To Do (\(Date(), formatter: self.dateFormatter))")
                 .toolbar {
                     ToolbarItem(placement: .navigationBarTrailing) {
                         Button(action: {
@@ -78,12 +158,96 @@ struct TodayView: View {
                     switch item {
                     case .settings:
                         CategoryFilterSheet(isFiltered: $isFiltered, filterCategory: filterCategory, userCategory: modelData.userCategory)
+                            .onDisappear {
+                                self.stateRoutineTodo = filteredRoutineTodo
+                                self.stateRoutineDone = filteredRoutineDone
+                                self.stateAdhocLogs = filteredAdhocLogs
+                            }
                     
                     case .profile:
                         ProfileHost()
                             .environmentObject(modelData)
                     }
                 }
+            }
+            .navigationViewStyle(StackNavigationViewStyle())
+        }
+        .onAppear {
+            self.stateRoutineTodo = filteredRoutineTodo
+            self.stateRoutineDone = filteredRoutineDone
+            self.stateAdhocLogs = filteredAdhocLogs
+        }
+    }
+    
+    func add(log: Log) {
+        let newLog = Log(context: self.moc)
+        newLog.id = UUID()
+        newLog.isToDo = false
+        newLog.isRoutine = true
+        newLog.routineLogId = log.id
+        newLog.category = log.category
+        newLog.categoryIcon = log.categoryIcon
+        newLog.name = log.name
+        newLog.activityDate = Date()
+        newLog.rating = Int16(3)
+//        newLog.comment = ""
+        newLog.subCategory = log.subCategory
+        newLog.subCategoryIcon = log.subCategoryIcon
+        newLog.creator = log.creator
+        newLog.desc = log.desc
+        newLog.amount = log.amount
+        newLog.unit = log.unit
+        newLog.updatedDate = Date()
+        newLog.status = "Worked"
+        newLog.image = log.image
+        stateRoutineDone.append(newLog)
+        
+        if self.moc.hasChanges {
+            do {
+                try self.moc.save()
+                print("New Log: \(log.wrappedName) added.")
+                
+            } catch {
+                print(error)
+            }
+        }
+
+    }
+    
+    func delete(log: Log) {
+        let index = stateRoutineDone.firstIndex(where: {$0.routineLogId == log.id})
+        let log = stateRoutineDone.first(where: {$0.routineLogId == log.id})
+        if log != nil {
+            print("Deleting a log...")
+            stateRoutineDone.remove(at: index!)
+            moc.delete(log!)
+        } else {
+            print("Cannot find the log to delete...")
+        }
+    
+        if self.moc.hasChanges {
+            do {
+                try self.moc.save()
+                print("Delete Log Completed.")
+            } catch {
+                print(error)
+            }
+        }
+    }
+    
+    func update(log: Log) {
+        if log.isToDo {
+            log.isToDo = false
+        } else {
+            log.isToDo = true
+        }
+        
+        if self.moc.hasChanges {
+            do {
+                try self.moc.save()
+                print("Log: \(log.name!) updated to \(log.isToDo).")
+            } catch {
+                print(error)
             }
         }
     }
