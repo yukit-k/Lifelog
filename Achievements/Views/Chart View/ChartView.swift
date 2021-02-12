b6eae622a8bca6c2cd34b51f1a5635e8de766c19
yuki678 <uk.12juni@gmail.com>
2021-02-12 05:57:38 +0000

    Modify the message display logic.

--- Achievements/Views/Chart View/ChartView.swift
+++ Achievements/Views/Chart View/ChartView.swift
@@ -8,24 +8,24 @@
 import SwiftUI
 import CoreData
 
-struct FilteredList<T: NSManagedObject, Content: View>: View {
-    var fetchRequest: FetchRequest<T>
-    var items: FetchedResults<T> { fetchRequest.wrappedValue }
-
-    // this is our content closure; we'll call this once for each item in the list
-    let content: (T) -> Content
-
-    var body: some View {
-        List(fetchRequest.wrappedValue, id: \.self) { item in
-            self.content(item)
-        }
-    }
-
-    init(filterKey: String, filterValue: String, @ViewBuilder content: @escaping (T) -> Content) {
-        fetchRequest = FetchRequest<T>(entity: T.entity(), sortDescriptors: [], predicate: NSPredicate(format: "%K BEGINSWITH %@", filterKey, filterValue))
-        self.content = content
-    }
-}
+//struct FilteredList<T: NSManagedObject, Content: View>: View {
+//    var fetchRequest: FetchRequest<T>
+//    var items: FetchedResults<T> { fetchRequest.wrappedValue }
+//
+//    // this is our content closure; we'll call this once for each item in the list
+//    let content: (T) -> Content
+//
+//    var body: some View {
+//        List(fetchRequest.wrappedValue, id: \.self) { item in
+//            self.content(item)
+//        }
+//    }
+//
+//    init(filterKey: String, filterValue: String, @ViewBuilder content: @escaping (T) -> Content) {
+//        fetchRequest = FetchRequest<T>(entity: T.entity(), sortDescriptors: [], predicate: NSPredicate(format: "%K BEGINSWITH %@", filterKey, filterValue))
+//        self.content = content
+//    }
+//}
 
 struct ChartView: View {
     @Environment(\.managedObjectContext) var moc
@@ -35,59 +35,112 @@ struct ChartView: View {
     @State private var isFiltered: Bool = false
     @StateObject var filterCategory = CategoryItem()
     
-    var fetchRequestHistory = FetchRequest<Log>(entity: Log.entity(),
-                        sortDescriptors: [],
-                        predicate: NSCompoundPredicate(type: .and, subpredicates: [
-                            NSPredicate(format: "activityDate >= %@", Date().addingTimeInterval(-60*60*24*60) as CVarArg),
-                            NSPredicate(format: "isToDo == false")
-                        ])
-    )
-    var logs: FetchedResults<Log> { fetchRequestHistory.wrappedValue }
-    var filteredLogs: [Log] {
+    func filter(_ logs: FetchedResults<Log>) -> [Log] {
         logs.filter { log in
             (isFiltered == false ||
                 isFiltered == true && log.wrappedCategory == filterCategory.category.name
             )
         }
     }
+    /* Today and Yesterday Part*/
+    @FetchRequest(entity: Log.entity(),
+                  sortDescriptors: [],
+                  predicate: NSCompoundPredicate(type: .or, subpredicates: [
+                      NSPredicate(format: "activityDate >= %@", Date().addingTimeInterval(-60*60*24*2) as CVarArg),
+                      NSPredicate(format: "NOT (isRoutine == true AND isToDo == true)")
+                  ])
+    ) var currentAdhocLogs: FetchedResults<Log>
     
-    var fetchRequestCurrent = FetchRequest<Log>(entity: Log.entity(),
-                        sortDescriptors: [],
-                        predicate: NSPredicate(format: "activityDate >= %@", Date().addingTimeInterval(-60*60*24*2) as CVarArg)
-    )
-    var curLogs: FetchedResults<Log> { fetchRequestCurrent.wrappedValue }
-    var filteredCurLogs: [Log] {
-        curLogs.filter { log in
-            (isFiltered == false ||
-                isFiltered == true && log.wrappedCategory == filterCategory.category.name
+    @FetchRequest(entity: Log.entity(),
+                  sortDescriptors: [],
+                  predicate: NSPredicate(format: "isRoutine == true AND isToDo == true")
+    ) var allRoutineLogs: FetchedResults<Log>
+    
+    var logsTodayDone: [Log] {
+        let todayStart = Calendar.current.startOfDay(for: Date())
+        let tomorrowStart = Calendar.current.date(byAdding: .day, value: 1, to: todayStart) ?? Date()
+        return filter(currentAdhocLogs).filter { log in
+            (log.wrappedActivityDate >= todayStart) &&
+                (log.wrappedActivityDate < tomorrowStart) &&
+                (log.isToDo == false)
+        }
+    }
+    var logsYesterdayDone: [Log] {
+        let todayStart = Calendar.current.startOfDay(for: Date())
+        let yesterdayStart = Calendar.current.date(byAdding: .day, value: -1, to: todayStart) ?? Date()
+        return filter(currentAdhocLogs).filter { log in
+            (log.wrappedActivityDate >= yesterdayStart) &&
+                (log.wrappedActivityDate < todayStart) &&
+                (log.isToDo == false)
+        }
+    }
+    var adhocLogsTodayToDo: [Log] {
+        let todayStart = Calendar.current.startOfDay(for: Date())
+        let tomorrowStart = Calendar.current.date(byAdding: .day, value: 1, to: todayStart) ?? Date()
+        return filter(currentAdhocLogs).filter { log in
+            (log.wrappedActivityDate >= todayStart) &&
+                (log.wrappedActivityDate < tomorrowStart) &&
+                (log.isToDo == true)
+        }
+    }
+    var adhocLogsYesterdayToDo: [Log] {
+        let todayStart = Calendar.current.startOfDay(for: Date())
+        let yesterdayStart = Calendar.current.date(byAdding: .day, value: -1, to: todayStart) ?? Date()
+        return filter(currentAdhocLogs).filter { log in
+            (log.wrappedActivityDate >= yesterdayStart) &&
+                (log.wrappedActivityDate < todayStart) &&
+                (log.isToDo == true)
+        }
+    }
+    var routineLogsToday: [Log] {
+        let todayStart = Calendar.current.startOfDay(for: Date())
+        let tomorrowStart = Calendar.current.date(byAdding: .day, value: 1, to: todayStart) ?? Date()
+        return filter(allRoutineLogs).filter { log in
+            (log.wrappedActivityDate < tomorrowStart) &&
+                (logsTodayDone.firstIndex(where: { $0.routineLogId == log.id }) == nil) &&
+                (isFiltered == false ||
+                    isFiltered == true && log.wrappedCategory == filterCategory.category.name
             )
         }
     }
-    
-    var thisMonth: String {
-        let components = Calendar.current.dateComponents([.year, .month], from: Date())
-        return String(components.year ?? 0) + "-" + String(format: "%02d", components.month ?? 0)
+    var routineLogsYesterday: [Log] {
+        let todayStart = Calendar.current.startOfDay(for: Date())
+        return filter(allRoutineLogs).filter { log in
+            (log.wrappedActivityDate < todayStart) &&
+                (logsYesterdayDone.firstIndex(where: { $0.routineLogId == log.id }) == nil) &&
+                (isFiltered == false ||
+                    isFiltered == true && log.wrappedCategory == filterCategory.category.name
+            )
+        }
     }
-    var prevMonth: String {
-        var dateComponent = DateComponents()
-        dateComponent.month = -1
-        let prevDate = Calendar.current.date(byAdding: dateComponent, to: Date())
-        let components = Calendar.current.dateComponents([.year, .month], from: prevDate!)
-        return String(components.year ?? 0) + "-" + String(format: "%02d", components.month ?? 0)
+    var logsTodayToDo: [Log] {
+        adhocLogsTodayToDo + routineLogsToday
+    }
+    var logsYesterdayToDo: [Log] {
+        adhocLogsYesterdayToDo + routineLogsYesterday
+    }
+
+    /* Weekly and Monthly Part*/
+    @FetchRequest(entity: Log.entity(),
+                  sortDescriptors: [],
+                  predicate: NSCompoundPredicate(type: .and, subpredicates: [
+                      NSPredicate(format: "activityDate >= %@", Date().addingTimeInterval(-60*60*24*60) as CVarArg),
+                      NSPredicate(format: "isToDo == false")
+                  ])
+    ) var historicalLogs: FetchedResults<Log>
+    
+    var logsByWeek: Dictionary<String, [Log]> {
+        Dictionary(grouping: filter(historicalLogs)) { (log) -> String in
+            let components = Calendar.current.dateComponents([.yearForWeekOfYear, .weekOfYear], from: log.date)
+            return String(components.yearForWeekOfYear ?? 0) + "-" + String(format: "%02d", components.weekOfYear ?? 0)
+        }
     }
     var logsByMonth: Dictionary<String, [Log]> {
-        Dictionary(grouping: filteredLogs) { (log) -> String in
+        Dictionary(grouping: filter(historicalLogs)) { (log) -> String in
             let components = Calendar.current.dateComponents([.year, .month], from: log.date)
             return String(components.year ?? 0) + "-" + String(format: "%02d", components.month ?? 0)
         }
-//        .map { (key: String, value: [Log]) -> (month: String, logs: [Log]) in
-//                (month: key, logs: value)
-//        }
-//        .sorted { (left, right) -> Bool in
-//            left.month < right.month
-//        }
     }
-    
     var thisWeek: String {
         let components = Calendar.current.dateComponents([.yearForWeekOfYear, .weekOfYear], from: Date())
         return String(components.yearForWeekOfYear ?? 0) + "-" + String(format: "%02d", components.weekOfYear ?? 0)
@@ -99,67 +152,86 @@ struct ChartView: View {
         let components = Calendar.current.dateComponents([.yearForWeekOfYear, .weekOfYear], from: prevDate!)
         return String(components.yearForWeekOfYear ?? 0) + "-" + String(format: "%02d", components.weekOfYear ?? 0)
     }
-    var logsByWeek: Dictionary<String, [Log]> {
-        Dictionary(grouping: filteredLogs) { (log) -> String in
-            let components = Calendar.current.dateComponents([.yearForWeekOfYear, .weekOfYear], from: log.date)
-            return String(components.yearForWeekOfYear ?? 0) + "-" + String(format: "%02d", components.weekOfYear ?? 0)
-        }
+    var thisMonth: String {
+        let components = Calendar.current.dateComponents([.year, .month], from: Date())
+        return String(components.year ?? 0) + "-" + String(format: "%02d", components.month ?? 0)
+    }
+    var prevMonth: String {
+        var dateComponent = DateComponents()
+        dateComponent.month = -1
+        let prevDate = Calendar.current.date(byAdding: dateComponent, to: Date())
+        let components = Calendar.current.dateComponents([.year, .month], from: prevDate!)
+        return String(components.year ?? 0) + "-" + String(format: "%02d", components.month ?? 0)
     }
     
     var dateFormatter: DateFormatter {
             let formatter = DateFormatter()
             formatter.dateStyle = .short
             return formatter
+    }
+//    var todayToDo: String {
+//        return dateFormatter.string(from: Date()) + "-ToDo"
+//    }
+//    var todayDone: String {
+//        return dateFormatter.string(from: Date()) + "-Done"
+//    }
+//    var yesterdayToDo: String {
+//        let yesterday = Calendar.current.date(byAdding: .day, value: -1, to: Date())
+//        return dateFormatter.string(from: yesterday!) + "-ToDo"
+//    }
+//    var yesterdayDone: String {
+//        let yesterday = Calendar.current.date(byAdding: .day, value: -1, to: Date())
+//        return dateFormatter.string(from: yesterday!) + "-Done"
+//    }
+//    var logsForCurrent: Dictionary<String, [Log]> {
+//        Dictionary(grouping: filteredCurLogs) { (log) -> String in
+//            let activityDate = dateFormatter.string(from: log.wrappedActivityDate)
+//            let toDoString = log.isToDo ? "ToDo" : "Done"
+//            return activityDate + "-" + toDoString
+//        }
+//    }
+    var chartMessageA: [LocalizedStringKey] {
+        var messages: [LocalizedStringKey] = []
+        for i in 0...9 {
+            messages.append(LocalizedStringKey("chartMessageA" + String(i)))
         }
-    var todayToDo: String {
-        return dateFormatter.string(from: Date()) + "-ToDo"
+        return messages
     }
-    var todayDone: String {
-        return dateFormatter.string(from: Date()) + "-Done"
-    }
-    var yesterdayToDo: String {
-        let yesterday = Calendar.current.date(byAdding: .day, value: -1, to: Date())
-        return dateFormatter.string(from: yesterday!) + "-ToDo"
-    }
-    var yesterdayDone: String {
-        let yesterday = Calendar.current.date(byAdding: .day, value: -1, to: Date())
-        return dateFormatter.string(from: yesterday!) + "-Done"
-    }
-    var logsForCurrent: Dictionary<String, [Log]> {
-        Dictionary(grouping: filteredCurLogs) { (log) -> String in
-            let activityDate = dateFormatter.string(from: log.wrappedActivityDate)
-            let toDoString = log.isToDo ? "ToDo" : "Done"
-            return activityDate + "-" + toDoString
+    var chartMessageB: [LocalizedStringKey] {
+        var messages: [LocalizedStringKey] = []
+        for i in 0...9 {
+            messages.append(LocalizedStringKey("chartMessageB" + String(i)))
         }
+        return messages
+    }
+    var chartMessageC: [LocalizedStringKey] {
+        var messages: [LocalizedStringKey] = []
+        for i in 0...9 {
+            messages.append(LocalizedStringKey("chartMessageC" + String(i)))
+        }
+        return messages
     }
 
     func getMessage(_ todayDone: Int, _ todaytoDo: Int, _ yesterdayDone: Int, _ yesterdayToDo: Int) -> LocalizedStringKey {
-        if todayDone == 0 {
-            if yesterdayDone == 0 {
-                return "chartMessage1"
-            } else if yesterdayDone < 2 {
-                return "chartMessage2"
-            } else {
-                return "chartMessage3"
-            }
-        } else if todayDone < 2 {
-            if yesterdayDone < 2 {
-                return "chartMessage4"
-            } else {
-                return "chartMessage5"
-            }
+        var dailyTarget = modelData.userProfile.dailyTarget
+        if dailyTarget == 0 {
+            dailyTarget = 5
+        }
+        let selectedIndex = min(Int((todayDone/dailyTarget)*5), 9)
+        if yesterdayDone < dailyTarget/2 {
+            return chartMessageA[selectedIndex]
+        } else if yesterdayDone < dailyTarget {
+            return chartMessageB[selectedIndex]
         } else {
-            return "chartMessage6"
+            return chartMessageC[selectedIndex]
         }
     }
             
     var body: some View {
-        let logsTodayToDo = logsForCurrent[todayToDo]
-        let logsTodayDone = logsForCurrent[todayDone]
-        let logsYesterdayToDo = logsForCurrent[yesterdayToDo]
-        let logsYesterdayDone = logsForCurrent[yesterdayDone]
-        
-        
+//        let logsTodayToDo = logsForCurrent[todayToDo] ?? []
+//        let logsTodayDone = logsForCurrent[todayDone] ?? []
+//        let logsYesterdayToDo = logsForCurrent[yesterdayToDo] ?? []
+//        let logsYesterdayDone = logsForCurrent[yesterdayDone] ?? []
         
         NavigationView {
             ScrollView {
@@ -174,7 +246,7 @@ struct ChartView: View {
                             .font(.system(size: 100))
                             .padding([.leading, .top], 20)
                         ChatBubble(direction: .left) {
-                            Text(getMessage(logsTodayDone?.count ?? 0, logsTodayToDo?.count ?? 0, logsYesterdayDone?.count ?? 0, logsYesterdayToDo?.count ?? 0))
+                            Text(getMessage(logsTodayDone.count, logsTodayToDo.count, logsYesterdayDone.count, logsYesterdayToDo.count))
                                 .padding(.all, 15)
                                 .foregroundColor(Color.white)
                                 .background(Color.blue)
@@ -185,16 +257,16 @@ struct ChartView: View {
                             Text("Today")
                                 .font(.title3)
                             HStack {
-                                CircleCount(title: "Done", count: logsTodayDone?.count ?? 0, width: 80,  color: .accentColor)
-                                CircleCount(title: "To Do", count: logsTodayToDo?.count ?? 0, width: 80,  color: .gray)
+                                CircleCount(title: "Done", count: logsTodayDone.count, width: 80,  color: .accentColor)
+                                CircleCount(title: "To Do", count: logsTodayToDo.count, width: 80,  color: .gray)
                             }
                         }
                         VStack {
                             Text("Yesterday")
                                 .font(.title3)
                             HStack {
-                                CircleCount(title: "Done", count: logsYesterdayDone?.count ?? 0, width: 80,  color: .green)
-                                CircleCount(title: "To Do", count: logsYesterdayToDo?.count ?? 0, width: 80,  color: logsYesterdayToDo?.count ?? 0 > 0 ? .red : .gray)
+                                CircleCount(title: "Done", count: logsYesterdayDone.count, width: 80,  color: .green)
+                                CircleCount(title: "To Do", count: logsYesterdayToDo.count, width: 80,  color: .gray)
                             }
                         }
                     }
