f56e47f06e62cc381971d15f6b05270e40a703cd
yuki678 <uk.12juni@gmail.com>
2021-02-15 08:43:16 +0000

    Add guide lines and fit the size of the monthly chart

--- Achievements/Views/Chart View/MonthlyChart.swift
+++ Achievements/Views/Chart View/MonthlyChart.swift
@@ -32,58 +32,87 @@ struct MonthlyChart: View {
 
     var body: some View {
         ScrollView(.horizontal, showsIndicators: false) {
-            HStack {
-                Spacer()
-                ForEach(0..<31) { day in
-                    VStack {
-                        HStack(spacing: 0) {
-                            Spacer()
-                            VStack(spacing: 0) {
+            ZStack(alignment: .bottom) {
+                HStack(spacing: 0) {
+                    Spacer()
+                    ForEach(0..<31) { day in
+                        VStack(spacing: 0) {
+                            HStack(spacing: 0) {
                                 Spacer()
-                                if self.countCumLogs(thisMonthLogs, day+1) > 0 {
-                                    Text("\(self.countCumLogs(thisMonthLogs, day+1))")
-                                        .font(.footnote)
-//                                        .rotationEffect(.degrees(-90))
-                                        .offset(y: self.countLogs(thisMonthLogs, day) > 0 ? 10 : -10)
-                                        .zIndex(1)
+                                VStack(spacing: 0) {
+                                    Spacer()
+                                    if self.countLogs(thisMonthLogs, day) == 1 {
+                                        Text("\(self.countLogs(thisMonthLogs, day))")
+                                            .font(.footnote)
+                                            .offset(y: -5)
+                                            .zIndex(2)
+                                    } else if self.countLogs(thisMonthLogs, day) > 1 {
+                                        Text("\(self.countLogs(thisMonthLogs, day))")
+                                            .font(.footnote)
+                                            .foregroundColor(.white)
+                                            //.rotationEffect(.degrees(-90))
+                                            .offset(y: CGFloat(self.countLogs(thisMonthLogs, day) - 1) * 5.0 + 12.0)
+                                            .zIndex(2)
+                                    }
+                                    Rectangle()
+                                        .fill(Color.accentColor)
+                                        .frame(width: 25, height: CGFloat(self.countLogs(thisMonthLogs, day)) * 10.0)
+                                    Rectangle()
+                                        .fill(Color.accentColor)
+                                        .opacity(0.2)
+                                        .frame(width: 25, height: CGFloat(self.countCumLogs(thisMonthLogs, day)) * 10.0)
                                 }
-                                if self.countLogs(thisMonthLogs, day) > 0 {
-                                    Text("\(self.countLogs(thisMonthLogs, day))")
-                                        .font(.footnote)
-                                        //.rotationEffect(.degrees(-90))
-                                        .offset(y: 17)
-                                        .zIndex(2)
+                                
+                                VStack(spacing: 0) {
+                                    Spacer()
+                                    Rectangle()
+                                        .fill(Color.green)
+                                        .frame(width: 25, height: CGFloat(self.countLogs(prevMonthLogs, day)) * 10.0)
+                                    Rectangle()
+                                        .fill(Color.green)
+                                        .opacity(0.2)
+                                        .frame(width: 25, height: CGFloat(self.countCumLogs(prevMonthLogs, day)) * 10.0)
                                 }
-                                Rectangle()
-                                    .fill(Color.accentColor)
-                                    .frame(width: 25, height: CGFloat(self.countLogs(thisMonthLogs, day)) * 10.0)
-                                Rectangle()
-                                    .fill(Color.accentColor)
-                                    .opacity(0.2)
-                                    .frame(width: 25, height: CGFloat(self.countCumLogs(thisMonthLogs, day)) * 10.0)
-                            }
-                            
-                            VStack(spacing: 0) {
                                 Spacer()
-                                Rectangle()
-                                    .fill(Color.green)
-                                    .frame(width: 25, height: CGFloat(self.countLogs(prevMonthLogs, day)) * 10.0)
-                                Rectangle()
-                                    .fill(Color.green)
-                                    .opacity(0.2)
-                                    .frame(width: 25, height: CGFloat(self.countCumLogs(prevMonthLogs, day)) * 10.0)
                             }
+                      
+                            Text("\(sequenceString(day+1))")
+                                .font(.footnote)
+                                .frame(height: 20)
+                                .padding(.top, 10)
+                        }
+                
+                    }
+                    Spacer()
+                }
+                .zIndex(1)
+                ForEach(0..<max(self.countCumLogs(prevMonthLogs, 31), self.countCumLogs(thisMonthLogs, 31))) { mark in
+                    if (mark % 5 == 0) {
+                        Rectangle()
+                            .fill(Color.gray)
+                            .offset(y: CGFloat(mark) * -10.0 - 30.0)
+                            .frame(height: 1.0)
+                        HStack{
+                            Text("\(mark)")
+                                .font(.footnote)
+                                .foregroundColor(.secondary)
                             Spacer()
                         }
-                  
-                        Text("\(day+1)")
-                            .font(.footnote)
-                            .frame(height: 20)
+                        .offset(y: CGFloat(mark) * -10.0 - 30.0)
                     }
-            
                 }
-                Spacer()
             }
+        }
+    }
+    func sequenceString(_ num: Int) -> String {
+        if [1, 21, 31].contains(num)  {
+            return "\(num)st"
+        } else if [2, 22].contains(num) {
+            return "\(num)nd"
+        } else if [3, 23].contains(num) {
+            return "\(num)rd"
+        } else {
+            return "\(num)th"
         }
     }
 }
